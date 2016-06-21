4a8355ffec407b2aa903845d1f18db72c91df822
jadrdc <jadrdc@gmail.com>
2016-06-21 19:08:50 -0400

    Primer Commit

---
+++ server.js
@@ -0,0 +1,65 @@
+var express=require("express");
+var morgan=require("morgan");
+var app=express();
+var mongoose=require('mongoose');
+var bodyParser=require('body-parser');
+var User = require('./models/user');
+var ejs=require('ejs');
+var ejsmate=require('ejs-mate');
+var mainroutes=require('./routes/main');
+var userroutes=require('./routes/user');
+var secret=require('./config/secret');
+
+var session=require('express-session');
+var cookieParser=require('cookie-parser');
+var flash=require('express-flash');
+
+
+var MongoStore=require('connect-mongo')(session);
+
+
+
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+app.engine('ejs',ejsmate);
+app.set('view engine','ejs');
+app.use(express.static(__dirname+'/public'));
+app.use(mainroutes);
+app.use(userroutes);
+app.use(cookieParser());
+
+app.use(
+session({
+
+  resave:true,
+  saveUnitialized:true,
+  secret:secret.secretkey,
+  store:new MongoStore({url:secret.database,autoReconnect:true})
+})
+
+);
+app.use(flash());
+
+
+
+
+
+
+
+mongoose.connect(secret.database,function(err)
+{
+	if (err)
+  {
+	   console.log('Not Connected');
+	}else
+  {
+	  console.log('Connected');
+	}});
+
+
+app.listen(secret.port,function(err)
+{
+	if(err)throw err;
+	console.log("The Server is Running");
+});
